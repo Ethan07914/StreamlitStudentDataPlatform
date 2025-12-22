6921379d58f07c3d226b19b58e53d93400aad4ba
EthanPook10 <Ethan.Pook10@outlook.com>
2025-12-22 11:53:09 +0000

    Made changes to base query to bring in occupation table columns

--- SQLQueries.py
+++ SQLQueries.py
@@ -1,8 +1,9 @@
 BaseQuery = \
 '''
-SELECT s.StudentID, sch.id, Sex, Age, AddressType, FamilySize, ParentLivingStatus, Commute, ReasonForSchoolChoice, TimeSpentStudying, FailureCount, EducationalSupport, ParentalSupport
+SELECT s.StudentID, sch.SchoolID, sch.Name AS SchoolName, Sex, Age, AddressType, FamilySize, ParentLivingStatus, Commute, ReasonForSchoolChoice, TimeSpentStudying, FailureCount, EducationalSupport, ParentalSupport
 , ReceivesTutoring, ExtraCurricular, AttendedNursery, PlansOnHigherEducation, HasInternet, InRelationship, FamilyRelationshipRating, BusynessScale, SocialScore, WeekdayAlcoholConsumption, WeekendAlcoholConsumption
-, HealthScore, AbsenceCount, MothersOccupation, MothersEducationLevel, FathersOccupation, FathersEducationLevel, 
+, HealthScore, AbsenceCount, GuardianOneId, MothersOccupationID, MothersOccupation, MothersEducationLevel, GuardianTwoID, FathersOccupationID, FathersOccupation, FathersEducationLevel, 
+IsFatherPrimary, IsMotherPrimary,
 CASE 
     WHEN IsFatherPrimary = 1 THEN 'Father'
     ELSE 'Mother'
@@ -10,17 +11,23 @@ END AS PrimaryGuardian,
 E1.Grade AS ExamOneGrade, E2.Grade AS ExamTwoGrade, E3.Grade AS ExamThreeGrade
     FROM Student AS s
     INNER JOIN 
-    (SELECT g.GuardianID, g.Occupation as MothersOccupation, g.EducationLevelID AS MothersEducationLevel, g.IsPrimary AS IsMotherPrimary
+    (SELECT g.GuardianID, g.OccupationID As MothersOccupationID, o.description as MothersOccupation,
+     g.EducationLevelID AS MothersEducationLevel, g.IsPrimary AS IsMotherPrimary
         FROM Guardian AS g
         INNER JOIN EducationLevel AS el
         ON g.EducationLevelID = el.EducationLevelID
+        INNER JOIN Occupation AS o
+        ON g.OccupationID = o.ID
             WHERE RelationshipToStudent = "Mother")
     AS m on s.GuardianOneID = m.GuardianID
     INNER JOIN 
-    (SELECT g.GuardianID, g.Occupation as FathersOccupation, g.EducationLevelID AS FathersEducationLevel, g.IsPrimary AS IsFatherPrimary
+    (SELECT g.GuardianID, g.OccupationID AS FathersOccupationID, o.Description as FathersOccupation,
+     g.EducationLevelID AS FathersEducationLevel, g.IsPrimary AS IsFatherPrimary
         FROM Guardian AS g
         INNER JOIN EducationLevel AS el
         ON g.EducationLevelID = el.EducationLevelID
+        INNER JOIN Occupation AS o
+        ON g.OccupationID = o.ID
             WHERE RelationshipToStudent = "Father")
     AS f on s.GuardianTwoID = f.GuardianID
     INNER JOIN School AS sch ON s.SchoolID = sch.SchoolID
