9623aed9a7e882dd3a61e057dbe4714c25a11d57
EthanPook10 <Ethan.Pook10@outlook.com>
2025-12-31 18:33:42 +0000

    feature = grade analysis, create class to analysis student grade performance by category and integrated this to frontend

---
+++ Analytics/Analysis.py
@@ -0,0 +1,37 @@
+import seaborn as sns
+
+
+class ExamThreeGradeAnalysis:
+    def __init__(self, BaseData, xcol, ycol='ExamThreeGrade'):
+        self.BaseData = BaseData
+        self.xcol = xcol
+        self.ycol = ycol
+        self.FilteredData = self.FilterData() #Setting the variable to the result of the function means these values are computed when a new object is initialised
+        self.MeanByCategory = self.GetMeanByCategory()
+
+    def FilterData(self):
+        return self.BaseData[[self.xcol, self.ycol]] #Filters out unnecessary
+
+    def PlotBox(self):
+        return sns.boxplot(self.FilteredData, x=self.xcol, y=self.ycol, hue=self.xcol, palette='Set1')
+
+    def PlotStrip(self):
+        return sns.stripplot(self.FilteredData, x=self.xcol, y=self.ycol, hue=self.xcol, palette='Set1')
+
+    def GetMeanByCategory(self):
+        Means = []
+        for value in self.FilteredData[self.xcol].unique(): #Iterates through each possible value in the columns
+            Category = self.FilteredData.loc[self.FilteredData[self.xcol] == value] #Locates all rows equal to that value
+            CategoryMean = Category[self.ycol].mean().round(2)
+            Means.append(float(CategoryMean)) #Appends the value as a float to the list rather than a numpy array
+        MeanDict = {'Category':list(self.FilteredData[self.xcol].unique()), 'Mean':Means} #Stores as key value pairs
+        return MeanDict
+
+    def PlotMeanAsBar(self):
+        barplot = sns.barplot(x=self.MeanByCategory['Category'], y=self.MeanByCategory['Mean'], hue=self.MeanByCategory['Category'], palette='Set1')
+        for container in barplot.containers:  # link: https://www.geeksforgeeks.org/python/how-to-show-values-on-seaborn-barplot/
+            barplot.bar_label(container) #Gives a label to each bar within the bar plot
+        return barplot
+
+
+
