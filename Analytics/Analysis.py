94f0afcdb738f8a5375001583d46d75a6561e89b
EthanPook10 <Ethan.Pook10@outlook.com>
2026-01-01 17:45:31 +0000

    feature = Grade analysis, Created another tab and tidied up code for tabs

--- Analytics/Analysis.py
+++ Analytics/Analysis.py
@@ -2,8 +2,9 @@ import seaborn as sns
 
 
 class ExamThreeGradeAnalysis:
-    def __init__(self, BaseData, xcol, ycol='ExamThreeGrade'):
+    def __init__(self, BaseData, palette, xcol, ycol='ExamThreeGrade'):
         self.BaseData = BaseData
+        self.palette = palette
         self.xcol = xcol
         self.ycol = ycol
         self.FilteredData = self.FilterData() #Setting the variable to the result of the function means these values are computed when a new object is initialised
@@ -13,10 +14,10 @@ class ExamThreeGradeAnalysis:
         return self.BaseData[[self.xcol, self.ycol]] #Filters out unnecessary
 
     def PlotBox(self):
-        return sns.boxplot(self.FilteredData, x=self.xcol, y=self.ycol, hue=self.xcol, palette='Set1')
+        return sns.boxplot(self.FilteredData, x=self.xcol, y=self.ycol, hue=self.xcol, palette=self.palette)
 
     def PlotStrip(self):
-        return sns.stripplot(self.FilteredData, x=self.xcol, y=self.ycol, hue=self.xcol, palette='Set1')
+        return sns.stripplot(self.FilteredData, x=self.xcol, y=self.ycol, hue=self.xcol, palette=self.palette)
 
     def GetMeanByCategory(self):
         Means = []
@@ -28,7 +29,7 @@ class ExamThreeGradeAnalysis:
         return MeanDict
 
     def PlotMeanAsBar(self):
-        barplot = sns.barplot(x=self.MeanByCategory['Category'], y=self.MeanByCategory['Mean'], hue=self.MeanByCategory['Category'], palette='Set1')
+        barplot = sns.barplot(x=self.MeanByCategory['Category'], y=self.MeanByCategory['Mean'], hue=self.MeanByCategory['Category'], palette=self.palette)
         for container in barplot.containers:  # link: https://www.geeksforgeeks.org/python/how-to-show-values-on-seaborn-barplot/
             barplot.bar_label(container) #Gives a label to each bar within the bar plot
         return barplot
--- pages/2_Analysis.py
+++ pages/2_Analysis.py
@@ -4,6 +4,7 @@ import pandas as pd
 from QueriesModule.SQLQueries import BaseQuery
 from Analytics.Analysis import ExamThreeGradeAnalysis
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.sidebar.title('Navigation')
 
@@ -13,46 +14,70 @@ con = sql.connect('studentperformance.db')
 curs = con.cursor()
 Data = pd.read_sql(BaseQuery, con)
 
-tab1, tab2, tab3 = st.tabs(['Grade Analysis by School', 'Grade Analysis by Higher Education Plans', 'Grade Analysis by Failure Count'])
+tab1, tab2, tab3, tab4 = st.tabs(['By School', 'By Higher Education Plans', 'By Failure Count', 'By Previous Exam Score'])
 
 with tab1:
- BySchoolBox = ExamThreeGradeAnalysis(Data, 'SchoolName').PlotBox()
+ BySchool = ExamThreeGradeAnalysis(Data, 'ocean', 'SchoolName') #object
+ #Palettes from, link: https://www.practicalpythonfordatascience.com/ap_seaborn_palette
+
+ BySchoolBox = BySchool.PlotBox()
  st.pyplot(BySchoolBox.figure)
+
  plt.clf() #Without this the plots are overlayed on the same chart (clears current figure)
- BySchoolStrip = ExamThreeGradeAnalysis(Data, 'SchoolName').PlotStrip()
+ BySchoolStrip = BySchool.PlotStrip()
  st.pyplot(BySchoolStrip.figure)
+
  plt.clf()
- BySchoolMeanComparison = ExamThreeGradeAnalysis(Data, 'SchoolName').PlotMeanAsBar()
+ BySchoolMeanComparison = BySchool.PlotMeanAsBar()
  plt.ylabel('Mean ExamThreeGrade')
  plt.xlabel('SchoolName')
  st.pyplot(BySchoolMeanComparison.figure)
 
 
 with tab2:
+ ByEducationPlans = ExamThreeGradeAnalysis(Data, 'magma', 'PlansOnHigherEducation') #object
+
  plt.clf()
- ByEducationPlansBox = ExamThreeGradeAnalysis(Data, 'PlansOnHigherEducation').PlotBox()
+ ByEducationPlansBox = ByEducationPlans.PlotBox()
  st.pyplot(ByEducationPlansBox.figure)
- plt.clf() #Without this the plots are overlayed on the same chart
- ByEducationPlansStrip = ExamThreeGradeAnalysis(Data, 'PlansOnHigherEducation').PlotStrip()
- st.pyplot(ByEducationPlansStrip.figure)
+
  plt.clf()
- ByEducationPlansMeanComparison = ExamThreeGradeAnalysis(Data, 'PlansOnHigherEducation').PlotMeanAsBar()
+ ByEducationPlansStrip = ByEducationPlans.PlotStrip()
+ st.pyplot(ByEducationPlansStrip.figure)
+
+ plt.clf()
+ ByEducationPlansMeanComparison = ByEducationPlans.PlotMeanAsBar()
  plt.ylabel('Mean ExamThreeGrade')
  plt.xlabel('PlansOnHigherEducation')
  st.pyplot(BySchoolMeanComparison.figure)
 
 with tab3:
+ ByFailures = ExamThreeGradeAnalysis(Data, 'plasma', 'FailureCount') #object
+
  plt.clf()
- ByFailuresBox = ExamThreeGradeAnalysis(Data, 'FailureCount').PlotBox()
+ ByFailuresBox = ByFailures.PlotBox()
  st.pyplot(ByFailuresBox.figure)
- plt.clf() #Without this the plots are overlayed on the same chart
- ByFailuresStrip = ExamThreeGradeAnalysis(Data, 'FailureCount').PlotStrip()
- st.pyplot(ByFailuresStrip.figure)
+
  plt.clf()
- ByFailuresMeanComparison = ExamThreeGradeAnalysis(Data, 'FailureCount').PlotMeanAsBar()
+ ByFailuresStrip = ByFailures.PlotStrip()
+ st.pyplot(ByFailuresStrip.figure)
+
+ plt.clf()
+ ByFailuresMeanComparison = ByFailures.PlotMeanAsBar()
  plt.ylabel('Mean ExamThreeGrade')
  plt.xlabel('FailureCount')
  st.pyplot(ByFailuresMeanComparison.figure)
+
+with tab4:
+ plt.clf()
+ ExamOne = ExamThreeGradeAnalysis(Data, None, 'ExamOneGrade').FilteredData.rename(columns={'ExamOneGrade': 'Grade'})
+ ExamOne['Exam'] = 'ExamOneGrade' #To allow for the hue
+
+ ExamTwo = ExamThreeGradeAnalysis(Data, None, 'ExamTwoGrade').FilteredData.rename(columns={'ExamTwoGrade': 'Grade'})
+ ExamTwo['Exam'] = 'ExamTwoGrade' #Creates Exam columns and sets value equal to ExamTwoGrade for every row
+
+ Exams = pd.concat([ExamOne, ExamTwo]) #Combines the two data frames
+ st.pyplot(sns.lmplot(data=Exams, x='Grade', y='ExamThreeGrade', hue='Exam', palette='prism').figure)
 
 
 
